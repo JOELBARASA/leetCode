fe3bb3b2e6939bf57822ac8ffd1af2934256fa43
JOELBARASA <barasajoel@gmail.com>
2022-01-31 23:15:32 +0300

    Update sorting-the-sentence.js

--- sorting-the-sentence.js
+++ sorting-the-sentence.js
@@ -2,22 +2,36 @@
  * @param {string} s
  * @return {string}
  */
-var sortSentence = function (s) {
-    console.log(s);
-    if (typeof s !== 'string' || s.length === 0) {
-        return "\nInvalid Entry!";
-    }
-    else {
+var areNumbersAscending = function (s) {
+    const split = s.split(' ');
+    let current = 0;
+    console.log(split);
 
-        let unOrderedSentence = s.split(' ');
-        let orderedSentence = new Array();
-        console.log(unOrderedSentence);
-        for (let i = 0; i < unOrderedSentence.length; i++) {
-            let strPos = unOrderedSentence[i].slice(-1);
-            let strValue = unOrderedSentence[i].substring(0, unOrderedSentence[i].indexOf(strPos));
-            orderedSentence[strPos] = strValue;
+    for (let i = 0; i < split.length; i++) {
+        const iValue = parseInt(split[i]);  
+        if (isNaN(iValue) === false) {           
+            if (iValue <= current) return false;
+            current = iValue;
         }
-        console.log(orderedSentence);
-        return orderedSentence.join(' ').trim();
     }
+    return true;
 };
+// var sortSentence = function (s) {
+//     console.log(s);
+//     if (typeof s !== 'string' || s.length === 0) {
+//         return "\nInvalid Entry!";
+//     }
+//     else {
+
+//         let unOrderedSentence = s.split(' ');
+//         let orderedSentence = new Array();
+//         console.log(unOrderedSentence);
+//         for (let i = 0; i < unOrderedSentence.length; i++) {
+//             let strPos = unOrderedSentence[i].slice(-1);
+//             let strValue = unOrderedSentence[i].substring(0, unOrderedSentence[i].indexOf(strPos));
+//             orderedSentence[strPos] = strValue;
+//         }
+//         console.log(orderedSentence);
+//         return orderedSentence.join(' ').trim();
+//     }
+// };
