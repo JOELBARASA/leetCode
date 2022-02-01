62be4a893e850e5993f7ee73e4e2c20b2ed556e5
JOELBARASA <barasajoel@gmail.com>
2022-02-01 11:57:18 +0300

    Update check-if-numbers-are-ascending-in-a-sentence.js
    https://leetcode.com/problems/check-if-numbers-are-ascending-in-a-sentence

--- check-if-numbers-are-ascending-in-a-sentence.js
+++ check-if-numbers-are-ascending-in-a-sentence.js
@@ -4,29 +4,48 @@
  */
 var areNumbersAscending = function (s) {
     if (typeof s !== 'string') {
-        return false;
+        return "\nInput not allowed! Please pass a valid string parameter.\n"
     }
     else {
-        console.log(s);
-        let comparativeArray = new Array();
-        let splitSentence = s.split(' ');
-        for (let i = 0; i < splitSentence.length; i++) {
-            if (isNaN(parseInt(splitSentence[i])) === false) {
-                console.log(parseInt(splitSentence[i]));
-                comparativeArray.push(parseInt(splitSentence[i]));
-            }
-        }
-
-        if (comparativeArray.length < 2) {
-            return false;
-        }
-        else {
-            for (let i = 0; i < comparativeArray.length; i++) {
-                if (comparativeArray[i] - comparativeArray[i + 1] >= 0) {
+        let split = s.split(' ');
+        console.log(split);
+        let previous = 0;
+        for (let i = 0; i < split.length; i++) {
+            // console.log(split[i] + ': ' + isNaN(split[i]));
+            if (isNaN(split[i]) === false) {
+                if (split[i] - previous <= 0)
                     return false;
-                }
+                previous = split[i];
             }
-            return true;
         }
+        return true;
     }
 };
+// var areNumbersAscending = function (s) {
+//     if (typeof s !== 'string') {
+//         return false;
+//     }
+//     else {
+//         console.log(s);
+//         let comparativeArray = new Array();
+//         let splitSentence = s.split(' ');
+//         for (let i = 0; i < splitSentence.length; i++) {
+//             if (isNaN(parseInt(splitSentence[i])) === false) {
+//                 console.log(parseInt(splitSentence[i]));
+//                 comparativeArray.push(parseInt(splitSentence[i]));
+//             }
+//         }
+
+//         if (comparativeArray.length < 2) {
+//             return false;
+//         }
+//         else {
+//             for (let i = 0; i < comparativeArray.length; i++) {
+//                 if (comparativeArray[i] - comparativeArray[i + 1] >= 0) {
+//                     return false;
+//                 }
+//             }
+//             return true;
+//         }
+//     }
+// };
