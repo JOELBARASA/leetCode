c7685ebffa4e612afca5cfa18c57a35d55bdacb1
JOELBARASA <barasajoel@gmail.com>
2022-02-04 21:09:06 +0300

    Update two-furthest-houses-with-different-colors.js

--- two-furthest-houses-with-different-colors.js
+++ two-furthest-houses-with-different-colors.js
@@ -1,21 +1,37 @@
-/**
- * @param {number[]} colors
- * @return {number}
- */
 var maxDistance = function (colors) {
     console.log(colors);
     if (!Array.isArray(colors) || colors.length < 2) {
         return 0;
     }
     else {
-        let maxDistance = 0;
-        for (let i = 0; i < colors.length; i++) {
-            for (let j = i + 1; j < colors.length; j++) {
-                if (colors[i] !== colors[j] && (j - i) > maxDistance) {
-                    maxDistance = j - i;
-                }
+        let left = 0, right = 1, maximumDisplacement = 0;
+        //Keep the left index starting and iterate to the right
+        //Incrementing the right index
+        while(right < colors.length){
+            if(colors[right] !== colors[left]){
+                maximumDisplacement = Math.max(maximumDisplacement, right - left);
             }
+            right++;
+        }        
+        //Repeat for the right index
+        right = colors.length - 1;
+        while(left < colors.length){
+            if(colors[right] !== colors[left]){
+                maximumDisplacement = Math.max(maximumDisplacement, right - left);
+            }
+            left++;
         }
-        return maxDistance;
+        return maximumDisplacement;
     }
+    // else {
+    //     let maxDistance = 0;
+    //     for (let i = 0; i < colors.length; i++) {
+    //         for (let j = i + 1; j < colors.length; j++) {
+    //             if (colors[i] !== colors[j] && (j - i) > maxDistance) {
+    //                 maxDistance = j - i;
+    //             }
+    //         }
+    //     }
+    //     return maxDistance;
+    // }
 };
